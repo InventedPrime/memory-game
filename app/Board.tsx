254beda28a2c22cf60f398a1f812728441fed0dd
killio99 <54195079+killio99@users.noreply.github.com>
2026-03-23 19:53:11 -0400

    changes

--- app/Board.tsx
+++ app/Board.tsx
@@ -11,12 +11,14 @@ const styles = StyleSheet.create({
     }
 })
 
-export default function Board({onPress}: ){
+export default function Board({ onPress }: { onPress: () => void}){
     return (
-        <Button onPress={onPress} color="red"/>
-        <Button onPress={onPress} color="red"/>
-        <Button onPress={onPress} color="red"/>
-        <Button onPress={onPress} color="red"/>
+        <>
+        <Button onPress={onPress} color="red" id={1}/>
+        <Button onPress={onPress} color="red" id={2}/>
+        <Button onPress={onPress} color="red" id={3}/>
+        <Button onPress={onPress} color="red" id={4}/>
+        </>
         
     )
 }
