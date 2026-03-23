b4ca1edd71244ff91359697e6e4d612caf5e3dff
InventedPrime <xenten51@gmail.com>
2026-03-23 19:59:45 -0400

    new changes

--- app/Board.tsx
+++ app/Board.tsx
@@ -1,24 +1,25 @@
 import Button from "./Button";
 
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 
 const styles = StyleSheet.create({
     Board: {
         flex: 1,
         borderColor: "black",
-        borderWidth: 2
+        borderWidth: 2,
+        flexDirection: 'row',
+        flexWrap: 'wrap'
     }
 })
 
 export default function Board({ onPress }: { onPress: () => void}){
     return (
-        <>
+        <View style={styles.Board}>
         <Button onPress={onPress} color="red" id={1}/>
         <Button onPress={onPress} color="red" id={2}/>
         <Button onPress={onPress} color="red" id={3}/>
         <Button onPress={onPress} color="red" id={4}/>
-        </>
-        
-    )
+        </View>
+    );
 }
--- app/Button.tsx
+++ app/Button.tsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet} from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
 
 type Props = {
     onPress: () => void;
@@ -8,13 +8,16 @@ type Props = {
 
 const styles = StyleSheet.create({
     Button: {
-        borderWidth: 2
+        borderWidth: 2,
+        flex: 1,
+        minHeight: 100,
+        margin: 5
     }
 })
 
 
 export default function Button({onPress, color, id}: Props){
     return (
-        <Pressable onPress={onPress} style={color} id={id}/>
+        <Pressable onPress={onPress} style={[styles.Button, {backgroundColor: color}]} key={id}/>
     )
 }
