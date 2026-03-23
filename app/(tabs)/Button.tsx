b91430b00ddee4211311adc9b3a634c84e38a5ba
killio99 <54195079+killio99@users.noreply.github.com>
2026-03-23 19:05:26 -0400

    button code

---
+++ app/(tabs)/Button.tsx
@@ -0,0 +1,14 @@
+import { Pressable, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    Button: {
+        borderWidth: 2
+    }
+})
+
+
+export default function Button(){
+    return (
+        
+    );
+}
