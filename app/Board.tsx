7f7aa8cd7e9809300f71a47b5a09df4ec8cc9cb6
killio99 <54195079+killio99@users.noreply.github.com>
2026-03-23 19:25:37 -0400

    fix

--- app/Board.tsx
+++ app/Board.tsx
@@ -2,15 +2,16 @@ import Button from "./Button";
 
 import { StyleSheet } from 'react-native';
 
+
 const styles = StyleSheet.create({
     Board: {
         flex: 1,
         borderColor: "black",
-        borderWidth: 2px
+        borderWidth: 2
     }
 })
 
-export default function Board(){
+export default function Board({onPress}: ){
     return (
         <Button onPress={onPress} color="red"/>
         <Button onPress={onPress} color="red"/>
